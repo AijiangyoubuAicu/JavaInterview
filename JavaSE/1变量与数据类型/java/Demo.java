1dfd05f6e608e8ab4f94f7083a2818a847865955
AijiangyoubuAicu <290743219@qq.com>
2019-09-02 22:45:35 +0800

    Merge branch 'master' of C:\Users\爱酱油不爱醋\Desktop\JavaInterview with conflicts.

--- JavaSE/1变量与数据类型/java/Demo.java
+++ JavaSE/1变量与数据类型/java/Demo.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
 public class Demo {
+    public static void main(String[] args) {
+        Integer i = 100, i2 = 100, i3 = 150, i4 = 150;
+        System.out.println(i == i2);
+        System.out.println(i3 == i4);
+    }
 }
